86e0835bc5e9b85c024d37bc84d2078b1435c5d4
Markus Klein <markus-klein@live.de>
2021-06-15 19:11:34 +0000

    forward fallback encodings

--- src/enum_args.rs
+++ src/enum_args.rs
@@ -1,5 +1,5 @@
-use anyhow::{bail, Error};
-use parquet::basic::Compression;
+use anyhow::{anyhow, bail, Error};
+use parquet::basic::{Compression, Encoding};
 use structopt::clap::arg_enum;
 
 arg_enum! {
@@ -52,4 +52,26 @@ pub fn compression_from_str(source: &str) -> Result<Compression, Error> {
         _ => bail!("Sorry, I do not know this compression identifier."),
     };
     Ok(compression)
+}
+
+pub fn encoding_from_str(source: &str) -> Result<Encoding, Error> {
+    let encoding = match source {
+        "plain" => Encoding::PLAIN,
+        // Causes error 'NYI("Encoding BIT_PACKED is not supported")'
+        //"bit-packed" => Encoding::BIT_PACKED,
+        "delta-binary-packed" => Encoding::DELTA_BINARY_PACKED,
+        "delta-byte-array" => Encoding::DELTA_BYTE_ARRAY,
+        "delta-length-byte-array" => Encoding::DELTA_LENGTH_BYTE_ARRAY,
+        "rle" => Encoding::RLE,
+        // ommitted, not a valid fallback encoding
+        //"rle-dictionary" => Encoding::RLE_DICTIONARY,
+        _ => bail!("Sorry, I do not know a column encoding called '{}'.", source)
+    };
+    Ok(encoding)
+}
+
+pub fn column_encoding_from_str(source: &str) -> Result<(String, Encoding), Error> {
+    let pos = source.rfind(':').ok_or(anyhow!("Column encoding must be parsed in format: 'COLUMN_NAME:ENCODING'"))?;
+    let (name, encoding) = source.split_at(pos);
+    Ok((name.to_owned(), encoding_from_str(&encoding[1..])?))
 }
