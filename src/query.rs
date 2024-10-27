ea579400ce3f2d9a8034407a95eadc6e95dcbbb7
Markus Klein <markus-klein@live.de>
2024-10-27 17:45:20 +0100

    refactor: introduce sequential fetch strategy

--- src/query.rs
+++ src/query.rs
@@ -17,7 +17,7 @@ mod timestamp_tz;
 use anyhow::Error;
 use io_arg::IoArg;
 use log::info;
-use odbc_api::{Cursor, IntoParameter};
+use odbc_api::{buffers::ColumnarAnyBuffer, BlockCursor, Cursor, IntoParameter};
 use std::io::{stdin, Read};
 
 use self::{
@@ -27,7 +27,7 @@ use self::{
     table_strategy::TableStrategy,
 };
 
-use crate::{open_connection, QueryOpt};
+use crate::{open_connection, parquet_buffer::ParquetBuffer, QueryOpt};
 
 /// Execute a query and writes the result to parquet.
 pub fn query(opt: QueryOpt) -> Result<(), Error> {
@@ -129,11 +129,54 @@ fn cursor_to_parquet(
     parquet_format_options: ParquetWriterOptions,
 ) -> Result<(), Error> {
     let table_strategy = TableStrategy::new(&mut cursor, mapping_options)?;
-    let odbc_buffer = table_strategy.allocate_fetch_buffer(batch_size)?;
-    let block_cursor = cursor.bind_buffer(odbc_buffer)?;
-    // let block_cursor = ConcurrentBlockCursor::from_block_cursor(block_cursor);
+    let fetch_strategy = SequentialFetch::new(cursor, &table_strategy, batch_size)?;
     let parquet_schema = table_strategy.parquet_schema();
     let writer = parquet_output(path, parquet_schema.clone(), parquet_format_options)?;
-    table_strategy.block_cursor_to_parquet(block_cursor, writer)?;
+    table_strategy.block_cursor_to_parquet(fetch_strategy, writer)?;
     Ok(())
+}
+
+/// Fetch one fetch buffer and write its contents to parquet. Then fill it again. This is not as
+/// fast as double buffering with concurrent fetching, but it uses less memory due to only requiring
+/// one fetch buffer.
+struct SequentialFetch<C: Cursor> {
+    block_cursor: BlockCursor<C, ColumnarAnyBuffer>,
+}
+
+impl<C> SequentialFetch<C>
+where
+    C: Cursor,
+{
+    pub fn new(
+        cursor: C,
+        table_strategy: &TableStrategy,
+        batch_size_limit: BatchSizeLimit,
+    ) -> Result<Self, Error> {
+        let mem_usage_odbc_buffer_per_row: usize = table_strategy.fetch_buffer_size_per_row();
+        let total_mem_usage_per_row =
+            mem_usage_odbc_buffer_per_row + ParquetBuffer::MEMORY_USAGE_BYTES_PER_ROW;
+        info!(
+            "Memory usage per row is {} bytes. This excludes memory directly allocated by the ODBC \
+            driver.",
+            total_mem_usage_per_row,
+        );
+
+        let batch_size_row = batch_size_limit.batch_size_in_rows(total_mem_usage_per_row)?;
+
+        info!("Batch size set to {} rows.", batch_size_row);
+
+        let fetch_buffer = table_strategy.allocate_fetch_buffer(batch_size_row);
+
+        let block_cursor = cursor.bind_buffer(fetch_buffer)?;
+        Ok(Self { block_cursor })
+    }
+
+    pub fn batch_size_in_rows(&self) -> usize {
+        self.block_cursor.row_array_size()
+    }
+
+    pub fn next_batch(&mut self) -> Result<Option<&ColumnarAnyBuffer>, odbc_api::Error> {
+        let batch = self.block_cursor.fetch()?;
+        Ok(batch)
+    }
 }
