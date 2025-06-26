793b73b9a42f1403b080337b981693a90f9a4580
Markus Klein <markus-klein@live.de>
2025-06-26 19:48:51 +0200

    refactor: IntroduceIndexMapping

--- src/execute.rs
+++ src/execute.rs
@@ -2,8 +2,11 @@ use std::{collections::HashMap, fs::File, mem::swap};
 
 use anyhow::{anyhow, Error};
 use log::info;
-use odbc_api::InOrder;
-use parquet::file::reader::{FileReader as _, SerializedFileReader};
+use odbc_api::InputParameterMapping;
+use parquet::{
+    file::reader::{FileReader as _, SerializedFileReader},
+    schema::types::SchemaDescriptor,
+};
 
 use crate::{
     connection::open_connection, input::parquet_type_to_odbc_buffer_desc,
@@ -22,41 +25,17 @@ pub fn execute(exec_opt: &ExecOpt) -> Result<(), Error> {
 
     let file = File::open(input)?;
     let reader = SerializedFileReader::new(file)?;
-
+    let (statement_text, placeholder_names_by_position) = to_positional_arguments(&statement);
     let parquet_metadata = reader.metadata();
     let schema_desc = parquet_metadata.file_metadata().schema_descr();
-    let num_columns = schema_desc.num_columns();
 
-    let (statement_text, mapping) = to_positional_arguments(&statement);
+    let mapping = IndexMapping::new(placeholder_names_by_position, schema_desc)?;
     let statement = odbc_conn.prepare(&statement_text)?;
 
-    let column_descriptions_by_name: HashMap<_, _> = (0..num_columns)
+    let parquet_column_descs_in_order_of_column_bufs: Vec<_> = mapping
+        .parquet_indices_in_order_of_column_buffers()
         .map(|index_pq| {
             let desc = schema_desc.column(index_pq);
-            (desc.name().to_owned(), (index_pq, desc))
-        })
-        .collect();
-
-    // Governs the relation between the indices of the positional placeholders in the SQL statement,
-    // the inidices of the ODBC transport buffer columns and the indices of the parquet columns.
-    //
-    // Buffer to Parquet index
-    let index_mappings: Vec<usize> = mapping
-        .iter()
-        .map(|name| {
-            column_descriptions_by_name
-                .get(name)
-                .map(|(index_pq, _)| *index_pq)
-                .ok_or_else(|| anyhow!("Parameter name {name} does not exist in parquet schema"))
-        })
-        .collect::<Result<_, Error>>()?;
-
-    let column_descriptionns_by_placeholder_index: Vec<_> = mapping
-        .iter()
-        .map(|name| {
-            let (_index_pq, desc) = column_descriptions_by_name.get(name).expect(
-                "A 'missing' column in the parquet schema, must be caught then building index map",
-            );
             Ok(desc)
         })
         .collect::<Result<_, Error>>()?;
@@ -65,8 +44,8 @@ pub fn execute(exec_opt: &ExecOpt) -> Result<(), Error> {
     // and the order of the columns in the allocated ODBC transport buffer. Yet, it may not be the
     // order of the columns in the parquet file, which is why we keep track of the index in parquet
     // separately.
-    let column_buf_desc: Vec<_> = column_descriptionns_by_placeholder_index
-        .into_iter()
+    let column_buf_desc: Vec<_> = parquet_column_descs_in_order_of_column_bufs
+        .iter()
         .map(|col_desc| {
             let (buf_desc, odbc_to_parquet) =
                 parquet_type_to_odbc_buffer_desc(col_desc, encoding.use_utf16())?;
@@ -81,7 +60,7 @@ pub fn execute(exec_opt: &ExecOpt) -> Result<(), Error> {
     let mut odbc_buffer = statement.into_column_inserter_with_mapping(
         batch_size,
         column_buf_desc.iter().map(|(desc, _copy_col)| *desc),
-        InOrder::new(index_mappings.len())
+        &mapping,
     )?;
 
     let mut pb = ParquetBuffer::new(batch_size);
@@ -109,10 +88,10 @@ pub fn execute(exec_opt: &ExecOpt) -> Result<(), Error> {
         }
         odbc_buffer.set_num_rows(num_rows);
         pb.set_num_rows_fetched(num_rows);
-        for (column_index, (_, parquet_to_odbc_col)) in column_buf_desc.iter().enumerate() {
-            let index_pq = index_mappings[column_index];
+        for (index_buf, index_pq) in mapping.parquet_indices_in_order_of_column_buffers().enumerate() {
             let column_reader = row_group_reader.get_column_reader(index_pq)?;
-            let column_writer = odbc_buffer.column_mut(column_index);
+            let column_writer = odbc_buffer.column_mut(index_buf);
+            let (_, parquet_to_odbc_col) = &column_buf_desc[index_buf];
             parquet_to_odbc_col(num_rows, &mut pb, column_reader, column_writer)?;
         }
 
@@ -120,6 +99,73 @@ pub fn execute(exec_opt: &ExecOpt) -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+// Governs the relation between the indices of the positional placeholders in the SQL statement,
+// the inidices of the ODBC transport buffer columns and the indices of the parquet columns.
+struct IndexMapping {
+    buffer_to_parquet_index: Vec<usize>,
+    parquet_index_by_name: HashMap<String, usize>,
+}
+
+impl IndexMapping {
+    /// # Parameters
+    ///
+    /// - `placeholder_names_by_position`: List of the placeholders. The index of the placeholder
+    ///   indicates its position in the query. The value is the name. The same name can appear
+    ///   multiple times.
+    /// - `schema_desc`: Schema descriptor of parquet file obtained from its metadata. It is used to
+    ///   reference the used placeholder names with the names of parquet columns and identify their
+    ///   indices within the parquet file.
+    pub fn new(
+        placeholder_names_by_position: Vec<String>,
+        schema_desc: &SchemaDescriptor,
+    ) -> Result<Self, Error> {
+        let num_columns = schema_desc.num_columns();
+        let parquet_index_by_name: HashMap<String, usize> = (0..num_columns)
+            .map(|index_pq| {
+                let desc = schema_desc.column(index_pq);
+                (desc.name().to_owned(), index_pq)
+            })
+            .collect();
+        let column_descriptions_by_name: HashMap<_, _> = (0..num_columns)
+            .map(|index_pq| {
+                let desc = schema_desc.column(index_pq);
+                (desc.name().to_owned(), (index_pq, desc))
+            })
+            .collect();
+        let buffer_to_parquet_index: Vec<usize> = placeholder_names_by_position
+            .iter()
+            .map(|name| {
+                column_descriptions_by_name
+                    .get(name)
+                    .map(|(index_pq, _)| *index_pq)
+                    .ok_or_else(|| {
+                        anyhow!("Parameter name {name} does not exist in parquet schema")
+                    })
+            })
+            .collect::<Result<_, Error>>()?;
+
+        Ok(IndexMapping {
+            buffer_to_parquet_index,
+            parquet_index_by_name,
+        })
+    }
+
+    /// Iterates over the parquet indices in order of the ODBC transparto buffers
+    pub fn parquet_indices_in_order_of_column_buffers(&self) -> impl Iterator<Item = usize> + '_ {
+        self.buffer_to_parquet_index.iter().copied()
+    }
+}
+
+impl InputParameterMapping for &IndexMapping {
+    fn parameter_index_to_column_index(&self, paramteter_index: u16) -> usize {
+        (paramteter_index - 1) as usize
+    }
+
+    fn num_parameters(&self) -> usize {
+        self.buffer_to_parquet_index.len()
+    }
 }
 
 /// Takes an SQL statement with named arguments and repalaces them with positional arguments.
